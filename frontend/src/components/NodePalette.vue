4b9632b7536cd33cc70184c106c9975d9d640fce
Art A. <Art9681@gmail.com>
2025-05-20 08:38:20 -0500

    feat: update styles in Header, NodePalette, and UtilityPalette components for consistency; remove unused styles in AgentNode component; update main entry file to use correct CSS

--- frontend/src/components/NodePalette.vue
+++ frontend/src/components/NodePalette.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="['fixed top-[62px] bottom-0 w-64 z-[1100] transition-transform duration-300 text-white dark:bg-neutral-800 flex flex-col space-y-2 shadow-[5px_0_10px_-5px_rgba(0,0,0,0.3)]', isOpen ? 'translate-x-0' : '-translate-x-full']">
-    <div class="absolute top-1/2 right-0 translate-x-full w-[30px] h-[60px] dark:bg-neutral-800 rounded-r-md flex items-center justify-center cursor-pointer shadow-[5px_0_10px_-5px_rgba(0,0,0,0.3)]" @click="togglePalette">
+  <div :class="['fixed top-[64px] bottom-0 w-64 z-[1100] transition-transform duration-300 text-white dark:bg-zinc-900 flex flex-col space-y-2 shadow-[5px_0_10px_-5px_rgba(0,0,0,0.3)]', isOpen ? 'translate-x-0' : '-translate-x-full']">
+    <div class="absolute top-1/2 right-0 translate-x-full w-[30px] h-[60px] dark:bg-zinc-900 rounded-r-md flex items-center justify-center cursor-pointer shadow-[5px_0_10px_-5px_rgba(0,0,0,0.3)]" @click="togglePalette">
       <svg v-if="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
       </svg>
@@ -8,7 +8,7 @@
         <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
       </svg>
     </div>
-    <div class="p-1 h-full box-border">
+    <div class="p-4 h-full box-border">
       <div class="overflow-y-auto h-full pr-2 space-y-4">
         <div v-for="(nodes, category) in nodeCategories" :key="category">
           <div class="text-base font-bold mb-2 uppercase text-gray-300 cursor-pointer flex justify-between items-center p-2 dark:bg-neutral-800 border border-white/20 rounded hover:bg-white/20" @click="toggleAccordion(category)">

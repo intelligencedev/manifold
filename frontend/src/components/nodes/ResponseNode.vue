95c7b68a1e40574035aba8af63e552c812bc0f51
Art A. <Art9681@gmail.com>
2025-05-21 07:39:48 -0500

    feat: Update BaseNode and ResponseNode components with improved layout and styling adjustments

--- frontend/src/components/nodes/ResponseNode.vue
+++ frontend/src/components/nodes/ResponseNode.vue
@@ -1,5 +1,5 @@
 <template>
-  <BaseNode :id="id" :data="data" :min-height="180" @resize="onResize">
+  <BaseNode :id="id" :data="data" :min-height="800" :min-width="704" @resize="onResize">
     <template #header>
       <div :style="data.labelStyle" class="node-label text-base font-semibold">
         {{ modelTypeLabel }}
